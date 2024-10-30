2117c32b57ffb45cb531076ffdb173768f46c6aa
thouseef-hamza <thouseefhamza.codes@gmail.com>
2024-10-30 16:54:06 +0530

    changed configuration for sql

--- src/configs/cors.config.js
+++ src/configs/cors.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  origin: process.env.PROJECT_URL,
+  origin: [
+    process.env.PROJECT_URL,     
+    'http://thousi.localhost:5173', 
+    'http://localhost:5173',       
+  ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
 };
--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const env = process.env;
 const { Sequelize } = require("sequelize");
 const logger = require("../../logger");
+const productionLogger = require("../../logger/production.logger");
 
 const db = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASSWORD, {
   host: env.DB_HOST,
-  dialect: "mysql",
-  logging: console.log,
+  dialect: env.DB_DIALECT,
+  logging: productionLogger,
 });
 
 const dbConnect = () => {

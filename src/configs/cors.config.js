f85dcde59a3010d12a664b7b6515acc477009c4c
Thouseef Hamza T P <119723781+thouseef-hamza@users.noreply.github.com>
2025-01-08 19:17:24 +0530

    Update cors.config.js

--- src/configs/cors.config.js
+++ src/configs/cors.config.js
@@ -1,7 +1,7 @@
 // CORS configuration
-const allowedOrigins = ["http://localhost:5173","https://nuzion-v2.vercel.app"];
+const allowedOrigins = ["http://127.0.0.1:5173","https://nuzion-v2.vercel.app"];
 const checkOrigin = (origin) => {
-  const regex = /^http:\/\/([a-zA-Z0-9-]+\.)?localhost:5173$/;
+  const regex = /^http:\/\/([a-zA-Z0-9-]+\.)?127.0.0.1:5173$/;
   return regex.test(origin);
 };
 
